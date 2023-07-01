ea1852288c02bbe40e8cae019eb87ba9f7f6b326
escowin <edwin@escowinart.com>
2023-07-01 16:07:51 -0500

    set up User document model

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// passwords strings are hashed before saving a user document
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
