bbc9aa87b490a052572906067c8052c6f2b28565
escowin <edwin@escowinart.com>
2023-07-09 19:46:15 -0500

    used let variable to update job query cache's notes array

--- client/src/components/NoteList.js
+++ client/src/components/NoteList.js
@@ -1,9 +1,37 @@
 import { useMutation } from "@apollo/client";
+import { QUERY_JOB } from "../utils/queries";
 import { DELETE_NOTE } from "../utils/mutations";
 
 function NoteList({ notes, jobId }) {
-  const [deleteNote, { error }] = useMutation(DELETE_NOTE);
+  let deletedNoteId;
+  const [deleteNote, { error }] = useMutation(DELETE_NOTE, {
+    update(cache, { data }) {
+      const { job } = cache.readQuery({
+        query: QUERY_JOB,
+        variables: { id: jobId },
+      });
+      // array is composed of objects that don't match the deleted note's id
+      const updatedNotes = job.notes.filter(
+        (note) => note._id !== deletedNoteId
+      );
+
+      // job query's notes array is written with the updated array
+      cache.writeQuery({
+        query: QUERY_JOB,
+        variables: { id: jobId },
+        data: {
+          job: {
+            ...job,
+            notes: updatedNotes,
+          },
+        },
+      });
+    },
+  });
+
   const handleDeleteNote = async (_id) => {
+    // value is set here in order to use the .filter() method to modify job query cache
+    deletedNoteId = _id;
     try {
       const { data } = await deleteNote({
         variables: { jobId: jobId, id: _id },
@@ -21,9 +49,9 @@ function NoteList({ notes, jobId }) {
           <p>{note.createdAt}</p>
           <p>{note.note}</p>
           <button onClick={() => handleDeleteNote(note._id)}>delete</button>
-          {error && <span>error</span>}
         </article>
       ))}
+      {error && <span>error</span>}
     </>
   );
 }
